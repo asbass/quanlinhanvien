f50f600949f1fb531204dee43d202dc6db893bcd
bui tan tai <93435375+asbass@users.noreply.github.com>
2024-10-03 03:56:06 +0000

    source /workspaces/quanlinhanvien/.venv/bin/activate

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈bạn tài cute")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
